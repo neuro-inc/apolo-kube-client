866e8fd652c741e72e3466b3e6d027b77d9e4a6a
Petrucci <petrucci@ua.fm>
2025-07-08 18:46:07 +0300

    add secrets api

--- apolo_kube_client/__init__.py
+++ apolo_kube_client/__init__.py
@@ -1,5 +1,27 @@
 from ._client import KubeClient
 from ._config import KubeConfig
 from ._core import KubeClientAuthType
+from ._errors import (
+    KubeClientException,
+    KubeClientExpired,
+    KubeClientUnauthorized,
+    ResourceBadRequest,
+    ResourceExists,
+    ResourceGone,
+    ResourceInvalid,
+    ResourceNotFound,
+)
 
-__all__ = ["KubeClient", "KubeConfig", "KubeClientAuthType"]
+__all__ = [
+    "KubeClient",
+    "KubeConfig",
+    "KubeClientAuthType",
+    "ResourceNotFound",
+    "ResourceExists",
+    "ResourceInvalid",
+    "ResourceBadRequest",
+    "ResourceGone",
+    "KubeClientException",
+    "KubeClientUnauthorized",
+    "KubeClientExpired",
+]
