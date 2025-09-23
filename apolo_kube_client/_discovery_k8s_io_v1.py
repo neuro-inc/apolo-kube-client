06e4fe389cd06b9386bb73006817a0f66249a054
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-09-23 16:26:36 +0200

    Switch to declarative attrs in kube entities description (#36)

--- apolo_kube_client/_discovery_k8s_io_v1.py
+++ apolo_kube_client/_discovery_k8s_io_v1.py
@@ -3,8 +3,15 @@ from kubernetes.client.models import (
     V1EndpointSliceList,
 )
 
+from ._attr import _Attr
 from ._base_resource import NamespacedResource
 from ._core import _KubeCore
+
+
+class EndpointSlice(
+    NamespacedResource[V1EndpointSlice, V1EndpointSliceList, V1EndpointSlice]
+):
+    query_path = "endpointslices"
 
 
 class DiscoveryK8sIoV1Api:
@@ -13,13 +20,7 @@ class DiscoveryK8sIoV1Api:
     """
 
     group_api_query_path = "apis/discovery.k8s.io/v1"
+    endpoint_slice = _Attr(EndpointSlice, group_api_query_path)
 
     def __init__(self, core: _KubeCore) -> None:
         self._core = core
-        self.endpoint_slice = EndpointSlice(core, self.group_api_query_path)
-
-
-class EndpointSlice(
-    NamespacedResource[V1EndpointSlice, V1EndpointSliceList, V1EndpointSlice]
-):
-    query_path = "endpointslices"
