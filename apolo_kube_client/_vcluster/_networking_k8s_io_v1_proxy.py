0ae3df977fbcb5d84667878d2f51a57425188f57
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-10-02 15:50:40 +0200

    Add ingress resource

--- apolo_kube_client/_vcluster/_networking_k8s_io_v1_proxy.py
+++ apolo_kube_client/_vcluster/_networking_k8s_io_v1_proxy.py
@@ -1,6 +1,12 @@
-from kubernetes.client.models import V1NetworkPolicy, V1NetworkPolicyList, V1Status
+from kubernetes.client.models import (
+    V1Ingress,
+    V1IngressList,
+    V1NetworkPolicy,
+    V1NetworkPolicyList,
+    V1Status,
+)
 
-from .._networking_k8s_io_v1 import NetworkingK8SioV1Api, NetworkPolicy
+from .._networking_k8s_io_v1 import Ingress, NetworkingK8SioV1Api, NetworkPolicy
 from ._attr_proxy import attr
 from ._resource_proxy import BaseProxy, NamespacedResourceProxy
 
@@ -13,6 +19,12 @@ class NetworkPolicyProxy(
     pass
 
 
+class IngressProxy(
+    NamespacedResourceProxy[V1Ingress, V1IngressList, V1Status, Ingress]
+):
+    pass
+
+
 class NetworkingK8SioV1ApiProxy(BaseProxy[NetworkingK8SioV1Api]):
     """
     NetworkK8sIo v1 API wrapper for Kubernetes.
@@ -21,3 +33,7 @@ class NetworkingK8SioV1ApiProxy(BaseProxy[NetworkingK8SioV1Api]):
     @attr(NetworkPolicyProxy)
     def network_policy(self) -> NetworkPolicy:
         return self._origin.network_policy
+
+    @attr(IngressProxy)
+    def ingress(self) -> IngressProxy:
+        return self._origin.ingress
