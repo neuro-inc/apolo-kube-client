06615e7a6bd1b0611626c42255dea57effd0234d
petrucci89 <petrucci@ua.fm>
2025-10-17 14:39:30 +0300

    add buckets crds support (#61)

--- apolo_kube_client/_vcluster/_neuromation_io_v1_proxy.py
+++ apolo_kube_client/_vcluster/_neuromation_io_v1_proxy.py
@@ -1,7 +1,19 @@
 from kubernetes.client.models import V1Status
 
-from .._crd_models import V1DiskNamingCRD, V1DiskNamingCRDList
-from .._neuromation_io_v1 import DiskNamingCRD, NeuromationioV1API
+from .._crd_models import (
+    V1DiskNamingCRD,
+    V1DiskNamingCRDList,
+    V1PersistentBucketCredentialCRD,
+    V1PersistentBucketCredentialCRDList,
+    V1UserBucketCRD,
+    V1UserBucketCRDList,
+)
+from .._neuromation_io_v1 import (
+    DiskNamingCRD,
+    NeuromationioV1API,
+    PersistentBucketCredentialCRD,
+    UserBucketCRD,
+)
 from ._attr_proxy import attr
 from ._resource_proxy import BaseProxy, NamespacedResourceProxy
 
@@ -9,6 +21,25 @@ from ._resource_proxy import BaseProxy, NamespacedResourceProxy
 class DiskNamingCRDProxy(
     NamespacedResourceProxy[
         V1DiskNamingCRD, V1DiskNamingCRDList, V1Status, DiskNamingCRD  # type: ignore
+    ]
+):
+    pass
+
+
+class UserBucketCRDProxy(
+    NamespacedResourceProxy[
+        V1UserBucketCRD, V1UserBucketCRDList, V1Status, UserBucketCRD  # type: ignore
+    ]
+):
+    pass
+
+
+class PersistentBucketCredentialCRDProxy(
+    NamespacedResourceProxy[
+        V1PersistentBucketCredentialCRD,  # type: ignore
+        V1PersistentBucketCredentialCRDList,  # type: ignore
+        V1Status,
+        PersistentBucketCredentialCRD,
     ]
 ):
     pass
@@ -22,3 +53,11 @@ class NeuromationioV1APIProxy(BaseProxy[NeuromationioV1API]):
     @attr(DiskNamingCRDProxy)
     def disk_naming(self) -> DiskNamingCRD:
         return self._origin.disk_naming
+
+    @attr(UserBucketCRDProxy)
+    def user_bucket(self) -> UserBucketCRD:
+        return self._origin.user_bucket
+
+    @attr(PersistentBucketCredentialCRDProxy)
+    def persistent_bucket_credential(self) -> PersistentBucketCredentialCRD:
+        return self._origin.persistent_bucket_credential
