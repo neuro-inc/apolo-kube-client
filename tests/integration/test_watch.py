8a502c3ab7e9984fcb4ebd65c11311c6e1ef01f7
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-11-25 14:02:32 +0100

    step

--- tests/integration/test_watch.py
+++ tests/integration/test_watch.py
@@ -30,7 +30,6 @@ class TestWatch:
                     node_event.type == "MODIFIED"
                     and node_event.object.metadata.labels["test"] == "test"
                 ):
-                    await asyncio.sleep(0.01)
                     break
 
         task = asyncio.create_task(_watch())
@@ -67,7 +66,7 @@ class TestWatch:
         events_count = 0
 
         async def _watch() -> None:
-            watch = kube_client.core_v1.node.watch()
+            watch = kube_client.core_v1.node.watch(allow_watch_bookmarks=True)
             started_event.set()
 
             async for node_event in watch.stream():
@@ -83,6 +82,8 @@ class TestWatch:
                     if events_count == max_events:
                         break
 
+            assert watch.resource_version is not None
+
         task = asyncio.create_task(_watch())
 
         await started_event.wait()
@@ -97,56 +98,3 @@ class TestWatch:
 
         async with asyncio.timeout(5):
             await task
-
-    async def test_update_resource_version(self, kube_client: KubeClient) -> None:
-        node_list = await kube_client.core_v1.node.get_list()
-        node: V1Node = node_list.items[0]
-        assert node.metadata.name is not None
-
-        try:
-            await kube_client.core_v1.node.patch_json(
-                node.metadata.name,
-                [{"op": "remove", "path": "/metadata/labels/test"}],
-            )
-        except Exception:
-            pass
-
-        started_event = asyncio.Event()
-
-        watch: Watch[V1Node] | None = None
-
-        async def _watch() -> None:
-            nonlocal watch
-            watch = kube_client.core_v1.node.watch(allow_watch_bookmarks=True)
-            started_event.set()
-
-            i = 0
-
-            async for node_event in watch.stream():
-                if (
-                    node_event.type == "MODIFIED"
-                    and "test" in node_event.object.metadata.labels
-                ):
-                    i += 1
-                    if i == 10:
-                        await asyncio.sleep(0.01)
-                        break
-
-        task = asyncio.create_task(_watch())
-
-        await started_event.wait()
-        assert watch is not None
-        assert watch.resource_version is None
-
-        for i in range(10):
-            # run multiple times, not all kube versions send BOOKMARK
-            # immediatelly after MODIFIED
-            await kube_client.core_v1.node.patch_json(
-                node.metadata.name,
-                [{"op": "add", "path": "/metadata/labels/test", "value": f"test{i}"}],
-            )
-
-        async with asyncio.timeout(5):
-            await task
-
-        assert watch.resource_version is not None
